993b7427627d86ea0b7d1e4a53ee83533f4751c7
cheng-chun-yuan <albert2367593@gmail.com>
2024-08-13 15:29:31 +0800

    feat(contarct): config setting and finsish contract instruction

--- programs/action-example/src/lib.rs
+++ programs/action-example/src/lib.rs
@@ -1,4 +1,11 @@
 use anchor_lang::prelude::*;
+use anchor_spl::associated_token::AssociatedToken;
+use anchor_spl::metadata::{
+    create_master_edition_v3, create_metadata_accounts_v3, CreateMasterEditionV3,
+    CreateMetadataAccountsV3, Metadata,
+};
+use anchor_spl::token::{mint_to, Mint, MintTo, Token, TokenAccount};
+use mpl_token_metadata::types::DataV2;
 
 declare_id!("BDwf8Mx2Cji2rNWCQ1atRsBMrDvcX485USgg7TVD3UjK");
 
@@ -6,11 +13,120 @@ declare_id!("BDwf8Mx2Cji2rNWCQ1atRsBMrDvcX485USgg7TVD3UjK");
 pub mod action_example {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn create_single_nft(
+        ctx: Context<CreateNFT>,
+        name: String,
+        symbol: String,
+        uri: String,
+    ) -> Result<()> {
+        mint_to(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                MintTo {
+                    authority: ctx.accounts.authority.to_account_info(),
+                    to: ctx.accounts.token_account.to_account_info(),
+                    mint: ctx.accounts.mint.to_account_info(),
+                },
+            ),
+            1, // 1 token
+        )?;
+    
+        create_metadata_accounts_v3(
+            CpiContext::new(
+                ctx.accounts.metadata_program.to_account_info(),
+                CreateMetadataAccountsV3 {
+                    payer: ctx.accounts.authority.to_account_info(),
+                    mint: ctx.accounts.mint.to_account_info(),
+                    metadata: ctx.accounts.nft_metadata.to_account_info(),
+                    mint_authority: ctx.accounts.authority.to_account_info(),
+                    update_authority: ctx.accounts.authority.to_account_info(),
+                    system_program: ctx.accounts.system_program.to_account_info(),
+                    rent: ctx.accounts.rent.to_account_info(),
+                },
+            ),
+            DataV2 {
+                name,
+                symbol,
+                uri,
+                seller_fee_basis_points: 0,
+                creators: None,
+                collection: None,
+                uses: None,
+            },
+            true,
+            true,
+            None,
+        )?;
+    
+        create_master_edition_v3(
+            CpiContext::new(
+                ctx.accounts.metadata_program.to_account_info(),
+                CreateMasterEditionV3 {
+                    edition: ctx.accounts.master_edition_account.to_account_info(),
+                    payer: ctx.accounts.authority.to_account_info(),
+                    mint: ctx.accounts.mint.to_account_info(),
+                    metadata: ctx.accounts.nft_metadata.to_account_info(),
+                    mint_authority: ctx.accounts.authority.to_account_info(),
+                    update_authority: ctx.accounts.authority.to_account_info(),
+                    system_program: ctx.accounts.system_program.to_account_info(),
+                    token_program: ctx.accounts.token_program.to_account_info(),
+                    rent: ctx.accounts.rent.to_account_info(),
+                },
+            ),
+            Some(1),
+        )?;
+    
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateNFT<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    #[account(
+        init,
+        payer = authority,
+        mint::decimals = 0,
+        mint::authority = authority,
+        mint::freeze_authority = authority,
+    )]
+    pub mint: Account<'info, Mint>,
+    #[account(
+        init_if_needed,
+        payer = authority,
+        associated_token::mint = mint,
+        associated_token::authority = authority,
+    )]
+    pub token_account: Account<'info, TokenAccount>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub metadata_program: Program<'info, Metadata>,
+    #[account(
+        mut,
+        seeds = [
+            b"metadata".as_ref(),
+            metadata_program.key().as_ref(),
+            mint.key().as_ref(),
+            b"edition".as_ref(),
+        ],
+        bump,
+        seeds::program = metadata_program.key()
+    )]
+    /// CHECK:
+    pub master_edition_account: UncheckedAccount<'info>,
+    #[account(
+        mut,
+        seeds = [
+            b"metadata".as_ref(),
+            metadata_program.key().as_ref(),
+            mint.key().as_ref(),
+        ],
+        bump,
+        seeds::program = metadata_program.key()
+    )]
+    /// CHECK:
+    pub nft_metadata: UncheckedAccount<'info>,
+}
--- tests/action-example.ts
+++ tests/action-example.ts
@@ -1,16 +1,70 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { ActionExample } from "../target/types/action_example";
+import { PublicKey, SystemProgram, Keypair } from "@solana/web3.js";
+import { ASSOCIATED_TOKEN_PROGRAM_ID, getAccount, TOKEN_PROGRAM_ID } from "@solana/spl-token";
+
 
 describe("action-example", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
+  const provider = anchor.getProvider();
+  const connection = provider.connection;
   const program = anchor.workspace.ActionExample as Program<ActionExample>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const confirm = async (signature: string): Promise<string> => {
+    const block = await connection.getLatestBlockhash();
+    await connection.confirmTransaction({
+      signature,
+      ...block,
+    });
+    return signature;
+  };
+
+  const log = async (signature: string): Promise<string> => {
+    console.log(
+      `Your transaction signature: https://explorer.solana.com/transaction/${signature}?cluster=devnet`
+    );
+    return signature;
+  };
+
+  const authority = anchor.Wallet.local() as anchor.Wallet;
+  const metadata = new anchor.web3.PublicKey("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s");
+  const mint = Keypair.generate();
+
+  const masterEditionAccount = PublicKey.findProgramAddressSync(
+    [Buffer.from("metadata"), metadata.toBuffer() , mint.publicKey.toBuffer(), Buffer.from("edition")],
+    metadata,
+  )[0];
+
+  const nftMetadata = PublicKey.findProgramAddressSync(
+    [Buffer.from("metadata"), metadata.toBuffer(), mint.publicKey.toBuffer()],
+    metadata,
+  )[0];
+
+  const accounts = {
+    authority: authority.publicKey,
+    payer: authority.publicKey,
+    mint: mint.publicKey,
+    associatedTokenprogram: ASSOCIATED_TOKEN_PROGRAM_ID,
+    tokenProgram: TOKEN_PROGRAM_ID,
+    systemProgram: SystemProgram.programId,
+    masterEditionAccount,
+    nftMetadata,
+  };
+
+
+  it("Create A NFT!", async () => {
+    const name =Buffer.from("Dappio Gang");
+    const symbol = Buffer.from("DAPG");
+    const url = Buffer.from("https://gateway.pinata.cloud/ipfs/QmWqzpzBGQyEi3CN4EcDDxKY7cU2Y2SXcuKESkfomG1KBy");
+    await program.methods
+      .createSingleNft(name.toString(), symbol.toString(),url.toString())
+      .accounts({ ...accounts })
+      .signers([mint])
+      .rpc()
+      .then(confirm)
+      .then(log);
   });
 });
